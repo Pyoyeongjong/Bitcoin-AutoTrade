fe09fd3092e7451c36eb1da307112edf78db4b16
Pyoyeongjong <pyjong1999@gmail.com>
2023-07-18 15:33:04 +0900

    Bye pycharm

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from binance.enums import *
 import win32api
 import time
 from datetime import datetime
+from datetime import timedelta
 
 # Dict 깔끔한 출력
 import pprint
@@ -29,6 +30,8 @@ import csv
 client = None
 # 현재 매수 중인지 확인하는 변수
 isOrdered = False
+is_Long = False
+is_Short = False
 
 # API 파일 경로
 api_key_file_path = "api.txt"
@@ -57,7 +60,31 @@ def set_system_time(serv_time):
                            0)
 
 # 선물 거래 ( Not Margin!! )
-def future_order(side, amount):
+def future_order(side, amount, sonjul):
+
+    global isOrdered
+
+    f_order = client.futures_create_order(
+        symbol='BTCUSDT',
+        side=side,
+        type=FUTURE_ORDER_TYPE_MARKET,
+        quantity=0.001
+    )
+
+    f_order = client.futures_create_order(
+        symbol='BTCUSDT',
+        side=SIDE_SELL,
+        type=FUTURE_ORDER_TYPE_STOP_MARKET,
+        stopPrice = sonjul,
+        quantity=0.001
+    )
+    if isOrdered is True:
+        isOrdered = False
+    else:
+        isOrdered = True
+    return f_order
+
+def future_order_close(side, amount):
     f_order = client.futures_create_order(
         symbol='BTCUSDT',
         side=side,
@@ -86,7 +113,7 @@ def get_usdt_balance(client):
     return usdt_balance
 
 # 선물 계좌 잔고/레버리지 출력
-def set_future_client_info(client, symbol, lev):
+def get_future_client_info(client, symbol, lev):
 
     global isOrdered
     leverage = None
@@ -95,11 +122,11 @@ def set_future_client_info(client, symbol, lev):
     usdt_balance = get_usdt_balance(client)
     # 레버리지 변경
     if isOrdered is False:
-        print(isOrdered)
+        #print(isOrdered)
         try:
             leverage_info = client.futures_change_leverage(symbol=symbol, leverage=lev)
             leverage = leverage_info['leverage']
-            print(f"레버리지: {leverage}")
+            #print(f"레버리지: {leverage}")
         except BinanceAPIException as e:
             print(e)
     else:
@@ -112,7 +139,7 @@ def set_future_client_info(client, symbol, lev):
     # 형 변환 / 최대 가용 사토시 계산
     if leverage is not None:
         satoshi = math.floor(float(usdt_balance)*float(leverage)/float(current_price) * 1000) / 1000
-    print(f"최대 매수(매도) 가능 BTC: {satoshi}")
+    #print(f"최대 매수(매도) 가능 BTC: {satoshi}")
     return leverage, satoshi
 
 # 캔들 기본 데이터
@@ -360,18 +387,29 @@ def spectate_bullish_divergence(candles, candles_info, top):
 def get_position(positions, symbol):
 
     global isOrdered
+    global is_Long
+    global is_Short
 
     for position in positions:
         if position['symbol'] == symbol:
             if float(position['positionAmt']) > 0:
                 print("현재 포지션 : Long")
                 isOrdered = True
+                is_Long = True
+                is_Short = False
+                return
             elif float(position['positionAmt']) < 0:
                 print("현재 포지션 : Short")
                 isOrdered = True
+                is_Long = False
+                is_Short = True
+                return
             else:
                 print("현재 포지션 : 없음")
                 isOrdered = False
+                is_Long = True
+                is_Short = False
+                return
 
 
 # 기울기 구하는 함수 # close는 Array, Numeric
@@ -380,6 +418,8 @@ def calculate_incline(close, i, j):
 
 # 1시간 봉 라이브 기울기 구하는 함수1
 def cal_inc_1h1d(ytdc, cp):
+    ytdc = float(ytdc)  # ytdc 변수를 부동소수점으로 변환
+    cp = float(cp)  # cp 변수를 부동소수점으로 변환
     return (cp-ytdc)/cp*1000
 
 # 1시간 봉 라이브 기울기 구하는 함수2
@@ -414,14 +454,21 @@ def calculate_trends(candles_info, start):
             continue
         inclination_mean = sum_inclination / count
         inclination_mean_list.append(inclination_mean)
-        timestamps.append(candles_info['Time'][i])  # Assuming 'Time' column exists in 'candles_info'
+        timestamps.append(candles_info['Time'][i])
 
-    return pd.DataFrame({'Inclination': inclination_mean_list})# 하루 데이터만 출력하도록 ( 임시 )
+    return pd.DataFrame({'Inclination': inclination_mean_list})
 
 def read_csv_data(time):
     candles_history = pd.read_csv(f"candle_data/candle_data_{time}.csv")
     return candles_history
 
+def add_tag(row):
+    if row['Inclination'] >= 8:
+        return '상승장'
+    elif row['Inclination'] >= -8:
+        return '횡보장'
+    else:
+        return '하락장'
 
 # sjh = 손절 상수, lev = 배율상수(전체시드 기준)
 def backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, inc, sjh, lev):
@@ -480,6 +527,7 @@ def backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, inc, s
             # 기울기 계산(1시간 봉 종가 기준마다)
             dayrow_open = dayrow['Open'].values[0]
             day_inclination = calculate_incline_1h_1d(dayrow_open, close, yest_inc) # 내적으로 보완. 당일 기울기 : 전일까지의 기울기평균 = 1:2
+            #print(dt, day_inclination)
             # 전일 기울기 업데이트
 
             # 예외 처리
@@ -587,6 +635,182 @@ def backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, inc, s
         f"  Current cash: {current_cash:<8.0f}  Highest cash: {highest_cash:<8.0f}  Lowest cash: {lowest_cash:<8.0f}"
     )
 
+def do_trading_hwengbo(inc, sjh, lev, client, symbol):
+
+
+    global isOrdered
+    global is_Long
+    global is_Short
+
+    is_hwengbo = False # 횡보 중인가?
+    is_Long = False
+    is_Short = False
+    is_zeongak = False
+
+    trade_count = 0 # 매매 횟수
+    win_count = 0 # 승리 횟수
+    lose_count = 0 # 패배 횟수
+
+    day_inclination = None # 현재 기울기
+    dayrow = None # 당일 데이터
+    yest_inc = None # 전일 기울기
+
+    in_price = 0 # 진입가
+    sonjul = 0 # 손절가
+
+
+    while True:
+
+        ### 캔들 정보 가져오기 (현재)
+        candles_1m, candles_5m, candles_15m, candles_1h, candles_4h, candles_1d, candles_1w = get_candles(client,
+                                                                                                          symbol,
+                                                                                                          limit)
+
+        ### 캔들 정보 가져오기 (특정 시각)
+        # start_time = datetime(2023, 5, 20)
+        # end_time = datetime(2023, 6, 26)
+        # candles_15m = get_klines_by_date(client, symbol, limit, Client.KLINE_INTERVAL_15MINUTE, start_time, end_time)
+
+        ### 보조지표 추출
+        candles_info_15m = get_candle_subdatas(candles_15m)
+        candles_info_1h = get_candle_subdatas(candles_1h)
+        candles_info_4h = get_candle_subdatas(candles_4h)
+        candles_info_1d = get_candle_subdatas(candles_1d)
+
+        row = candles_info_1h.iloc[-1]
+        upperband = float(row['UpperBand'])
+        lowerband = float(row['LowerBand'])
+        close = float(row['Close'])
+        volume = row['Volume']
+        vol_sma = row['Vol_SMA']
+        low = row['Low']
+        high = row['High']
+        dt = row['Time']
+
+        now = datetime.now()
+
+        if now.minute == 0 and now.second<3:
+            is_zeongak = True
+        else:
+            is_zeongak = False
+
+        dt_day = None
+
+        if dt.hour >=9:
+            dt_day = dt.replace(hour=9, minute=0, second=0, microsecond=0)
+        else:
+            dt = dt - timedelta(days=1, hours=dt.hour, minutes=dt.minute, seconds=dt.second, microseconds=dt.microsecond)
+            dt_day = dt.replace(hour=9, minute=0, second=0, microsecond=0)
+
+        dt_yest = dt - timedelta(days=2, hours=dt.hour, minutes=dt.minute, seconds=dt.second, microseconds=dt.microsecond)
+        dt_yest = dt.replace(hour=9, minute=0, second=0, microsecond=0)
+
+        # 사지 않은 상태라면,
+        if not isOrdered:
+
+            leverage, satoshi = get_future_client_info(client, symbol, lev)
+
+            amount = satoshi * leverage * 0.5
+
+            # 전날 일봉 기울기 가져오기
+            yestrow = candles_info_1d[candles_info_1d['Time'] == dt_yest]
+            yest_inc = yestrow['Inclination'].values[0]
+            # 당일 일봉 OPEN 값 가져오기
+            dayrow = candles_info_1d[candles_info_1d['Time'] == dt_day]
+            dayrow_open = dayrow['Open'].values[0]
+            # 기울기 계산(1시간 봉 종가 기준마다)
+            day_inclination = calculate_incline_1h_1d(dayrow_open, close, yest_inc) # 내적으로 보완. 당일 기울기 : 전일까지의 기울기평균 = 1:2
+            # 전일 기울기 업데이트
+
+            # 기울기 = 횡보일 때
+            if -inc < day_inclination < inc:
+                is_hwengbo = True
+            else:
+                return
+
+            # 횡보라고 판정됨 -> 매수 준비
+            if is_hwengbo:
+                # 1시간 봉 기준 볼린저밴드 아래에서 마감
+                if lowerband > close:
+                    # 거래량이 평균보다 20% 높으면 롱 진입.
+                    if (volume > vol_sma * 1.2).all():
+                        in_price = close
+                        is_bought = True
+                        sonjul = close - close * 0.01 * sjh
+                        is_Long = True
+                        is_Short = False
+                        future_order('BUY', amount, sonjul)
+                        print(f"롱. 양:{amount}, 손절:{sonjul}")
+
+                # 1시간 봉 기준 볼린저밴드 위에서 마감
+                if close > upperband:
+                    # 거래량이 평균보다 20% 높으면 숏 진입.
+                    if (volume > vol_sma * 1.2).all():
+                        in_price = close
+                        is_bought = True
+                        sonjul = close + close * 0.01 * sjh
+                        is_Long = False
+                        is_Short = True
+                        trade_count += 1
+                        #print("오늘 기울기: ", day_inclination)
+                        future_order('SELL', amount, sonjul)
+                        print(f"숏. 양:{amount}, 손절:{sonjul}")
+
+        else:
+            if is_Long:
+                # 익절
+                if low <= upperband <= high:
+                    is_bought = False
+                    suik = current_cash * (upperband / in_price - 1-0.0008) * lev * 0.5 # 수익 계산
+                    current_cash += suik
+                    if current_cash > highest_cash:
+                        highest_cash = current_cash
+                    elif current_cash < lowest_cash:
+                        lowest_cash = current_cash
+                    win_count += 1
+                    future_order_close('SELL', amount)
+                    print("익절")
+
+                # 손절
+                elif low <= sonjul <= high:
+                    is_bought = False
+                    current_cash += current_cash * (sonjul / in_price - 1-0.0008) * lev* 0.5
+                    if current_cash > highest_cash:
+                        highest_cash = current_cash
+                    elif current_cash < lowest_cash:
+                        lowest_cash = current_cash
+                    lose_count += 1
+                    future_order_close('SELL', amount)
+                    print("손절")
+
+            elif is_Short:
+                # 익절
+                if low <= lowerband <= high:
+                    is_bought = False
+                    suik = current_cash * (in_price / lowerband - 1-0.0008) * lev * 0.5 # 수익 계산
+                    current_cash += suik
+                    if current_cash > highest_cash:
+                        highest_cash = current_cash
+                    elif current_cash < lowest_cash:
+                        lowest_cash = current_cash
+                    win_count += 1
+                    future_order_close('BUY', amount)
+                    print("익절")
+
+                # 손절
+                elif low <= sonjul <= high:
+                    is_bought = False
+                    current_cash += current_cash * (in_price / sonjul - 1-0.0008) * lev * 0.5
+                    if current_cash > highest_cash:
+                        highest_cash = current_cash
+                    elif current_cash < lowest_cash:
+                        lowest_cash = current_cash
+                    lose_count += 1
+                    future_order_close('BUY', amount)
+                    print("손절")
+
+        print(f"보류. 기울기 : {day_inclination}")
+        time.sleep(3)
 
 def backTesting_bull():
 
@@ -600,7 +824,7 @@ if __name__ == '__main__':
 
     ### Initiation
     # row 생략 없이 출력
-    pd.set_option('display.max_rows', 10)
+    pd.set_option('display.max_rows', None)
     # col 생략 없이 출력
     pd.set_option('display.max_columns', None)
     # 캔들 데이터 가져오기
@@ -610,6 +834,7 @@ if __name__ == '__main__':
     satoshi = None
     leverage = None
 
+    time1 = time.time()
     ### 계좌 연결
     binance_access_key, binance_secret_key = read_api_keys(api_key_file_path)
     try:
@@ -626,46 +851,46 @@ if __name__ == '__main__':
     get_position(positions, symbol)
     ### Client 정보 설정 및 잔고 출력
     get_usdt_balance(client)
-    #leverage, satoshi = set_future_client_info(client, symbol, 3) # 현재 거래 중일 시 레버리지 움직이면 오류.
+    leverage, satoshi = get_future_client_info(client, symbol, 10) # 현재 거래 중일 시 레버리지 움직이면 오류.
+    print(leverage, satoshi)
 
-    ### 캔들 정보 가져오기 (현재)
+
+    # ### 캔들 정보 가져오기 (현재)
     # candles_1m, candles_5m, candles_15m, candles_1h, candles_4h, candles_1d, candles_1w = get_candles(client, symbol, limit)
-
-    ### 캔들 정보 가져오기 (특정 시각)
-    # start_time = datetime(2023, 5, 20)
-    # end_time = datetime(2023, 6, 26)
-    # candles_15m = get_klines_by_date(client, symbol, limit, Client.KLINE_INTERVAL_15MINUTE, start_time, end_time)
-
-    ### 보조지표 추출
+    #
+    # ### 캔들 정보 가져오기 (특정 시각)
+    # # start_time = datetime(2023, 5, 20)
+    # # end_time = datetime(2023, 6, 26)
+    # # candles_15m = get_klines_by_date(client, symbol, limit, Client.KLINE_INTERVAL_15MINUTE, start_time, end_time)
+    #
+    # ### 보조지표 추출
     # candles_info_15m = get_candle_subdatas(candles_15m)
     # candles_info_1h = get_candle_subdatas(candles_1h)
     # candles_info_4h = get_candle_subdatas(candles_4h)
     # candles_info_1d = get_candle_subdatas(candles_1d)
-    # print(candles_info_1d)
+    # time2 = time.time()
 
     ### 과거 데이터 & 보조지표 (Timestamp 안 이상함)
-    time0 = time.time()
-
-    candles_history_15m = read_csv_data("15m")
-    candles_history_info_15m = get_candle_subdatas(candles_history_15m)
-
-    candles_history_1h = read_csv_data("1h")
-    candles_history_info_1h = get_candle_subdatas(candles_history_1h)
-
-    candles_history_4h = read_csv_data("4h")
-    candles_history_info_4h = get_candle_subdatas(candles_history_4h)
-
-    candles_history_1d = read_csv_data("1d")
-    candles_history_info_1d = get_candle_subdatas(candles_history_1d)
-
-    candles_history_1h_21 = pd.read_csv(f"candle_data/candle_data_1h_before_21.csv")
-    candles_history_info_1h_21 = get_candle_subdatas(candles_history_1h_21)
-
-    candles_history_4h_21 = pd.read_csv(f"candle_data/candle_data_4h_before_21.csv")
-    candles_history_info_4h_21 = get_candle_subdatas(candles_history_4h_21)
-
-    candles_history_1d_21 = pd.read_csv(f"candle_data/candle_data_1d_before_21.csv")
-    candles_history_info_1d_21 = get_candle_subdatas(candles_history_1d_21)
+    # candles_history_15m = read_csv_data("15m")
+    # candles_history_info_15m = get_candle_subdatas(candles_history_15m)
+    #
+    # candles_history_1h = read_csv_data("1h")
+    # candles_history_info_1h = get_candle_subdatas(candles_history_1h)
+    #
+    # candles_history_4h = read_csv_data("4h")
+    # candles_history_info_4h = get_candle_subdatas(candles_history_4h)
+    #
+    # candles_history_1d = read_csv_data("1d")
+    # candles_history_info_1d = get_candle_subdatas(candles_history_1d)
+    #
+    # candles_history_1h_21 = pd.read_csv(f"candle_data/candle_data_1h_before_21.csv")
+    # candles_history_info_1h_21 = get_candle_subdatas(candles_history_1h_21)
+    #
+    # candles_history_4h_21 = pd.read_csv(f"candle_data/candle_data_4h_before_21.csv")
+    # candles_history_info_4h_21 = get_candle_subdatas(candles_history_4h_21)
+    #
+    # candles_history_1d_21 = pd.read_csv(f"candle_data/candle_data_1d_before_21.csv")
+    # candles_history_info_1d_21 = get_candle_subdatas(candles_history_1d_21)
 
     ### 하락 다이버전스 발견(과거 데이터)(리스트 형식) 출력 = [(time1, time2)]
     # print(detect_bullish_divergences(candles_15m, candles_info_15m, 70))
@@ -677,39 +902,22 @@ if __name__ == '__main__':
     # print(spectate_bullish_divergence(candles_15m, candles_info_15m, 70))
 
     ### 장 추세 계산함수 (일봉 9.0, 4시간봉 1.5, 1시간봉 0.3) 오늘 계산 = len(candles)-1
-    # incinc = pd.concat([candles_history_info_1d['Time'], candles_history_info_1d['Inclination']],axis=1)
-    # filtered_df = incinc[incinc.iloc[:, 1] > 10]
-    # print(filtered_df)
+    # incinc = pd.concat([candles_history_info_4h['Time'], candles_history_info_4h['Inclination']],axis=1)
+    # incinc['Tag'] = incinc.apply(add_tag, axis=1)
     # print(incinc)
 
-    #print(candles_history_info_1d)
+    # filtered_df = incinc[incinc.iloc[:, 1] > 10]
+    # print(filtered_df)
+
 
     ### 함수 걸리는 시간 계산
     # time1 = time.time()
     # time2 = time.time()
     # print(time2-time1)
 
-    # 횡보장 전략 확정!!! 기울기 -10~10, 손실 2%, 레버리지 10배, 기울기 보수지표 = 1
-    print("History 18~20\n")
+    ### 횡보장 전략 확정 기울기 -5~5, 손실 2%, 레버리지 10배, 기울기 보수지표 = 1
+    # backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, 5, 2, 10)
 
-    for i in range(1,21):
-        print(f"[[[{i}]]]")
-        backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, i, 0.5, 20)
-        backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, i, 0.66, 15)
-        backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, i, 1, 10)
-        backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, i, 2, 5)
-        backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, i, 3, 4)
-        backTesting_hwengbo(candles_history_info_1h_21, candles_history_info_1d_21, i, 4, 3)
-
-    print("\n\nHistory 21~\n")
-
-    for i in range(1,21):
-        print(f"[[[{i}]]]")
-        backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, i, 0.5, 20)
-        backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, i, 0.66, 15)
-        backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, i, 1, 10)
-        backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, i, 2, 5)
-        backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, i, 3, 4)
-        backTesting_hwengbo(candles_history_info_1h, candles_history_info_1d, i, 4, 3)
-
+    #do_trading_hwengbo(5, 2, leverage, client, symbol)
+    future_order("BUY", 0.001, '30100')
 
